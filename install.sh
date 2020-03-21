c1d972d7a10245b7e1bb089a7533458609966e3b
Wayne Gramlich <wayne@gramlich.net>
2020-03-21 16:09:05 -0700

    Updated install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,12 @@ set -e           # Exit immediately on error result.
 set -o pipefail  # Fail if any commands in a pipeline fail.
 set -u           # Treat unset variables as an error.
 
-# Use a macro for invoking `sudo`:
-SUDO=sudo -H
-INSTALL=$SUDO apt install
-PIP_INSTALL=pip install
+# Some commonly used macros:
+SUDO="sudo -H"
+APT_ADD_REPOSITORY="sudo add-apt-repository --yes"
+APT_INSTALL="$SUDO apt install --yes"
+APT_UPDATE="$SUDO apt update"
+PIP_INSTALL="pip install"
 
 # Capture the REPOS directory root:
 REPOS=`(cd .. ; pwd)`
@@ -21,52 +23,68 @@ REPOS=`(cd .. ; pwd)`
 echo "Installing additional packages.  You may be prompted for your root password ..."
 
 # Make sure we have the build-essential package installed.
-if [ ! -n `which make` ]
+if [ -z `which make` ]
     then
         echo "Installing build-essential package ..."
-        $INSTALL build-essential
+        $APT_INSTALL build-essential
+    else
+        echo "build-essential previously installed"
     fi
 
 # Make sure we have openscad installed:
-if [ ! -n `which openscad` ]
+if [ -z `which openscad` ]
     then
+        # While there is no standard openscad package for 18.04 or 18.10, there is one for 19.10.
+	# We side step the issue by just grabbing openscad directly from the openscad repository.
         echo "Installing openscad package ..."
-        $INSTALL openscad
+        $APT_ADD_REPOSITORY ppa:openscad/releases
+        $APT_UPDATE
+        $APT_INSTALL openscad
+    else
+        echo "openscad previously installed"
     fi
     
-# Make sure we have qcad installed:
-if [ ! -n `which qcad` ]
+# Install python3:
+if [ -z `which python3` ]
+    then
+        echo "Installing Python 3 ..."
+	$APT_INSTALL python3
+    else
+        echo "Python3 previously installed"
+    fi
+
+# Install Python virtual environments:
+if [ ! -n `which virtuaenvwrapper.sh` ]
+    then
+        echo "Installing Python virtual environments ..."
+	$PIP_INSTALL virtualenvwrapper
+    else
+        echo "Python virtual environments previously installed"
+    fi
+
+# Install qcad:
+if [ -z `which qcad` ]
     then
         echo "Installing qcad package ..."
-	$INSTALL qcad
+	$APT_ADD_REPOSITORY ppa:alex-p/qcad
+	$APT_UPDATE
+	$APT_INSTALL qcad
+    else
+        echo "qcad previously installed"
     fi
 
 # Install kicad:
-if [ ! n `which kicad` ]
+if [ -z `which kicad` ]
     then
         echo "Installing kicad electronic computer aided design package ..."
 	echo "Add KiCad to your package repository database:"
         $SUDO add-apt-repository --yes ppa:js-reynaud/kicad-5.1
 	echo "Update your package repository database:"
-        $SUDO sudo apt update
+        $APT_UPDATE
 	echo "Install kicad. This over a gigabyte of code/libraires to download ..."
-        $INSTALL --install-recommends kicad
+        $APT_INSTALL --install-recommends kicad
 	echo "Install KiCad demo projects:"
-        $INSTALL kicad-demo
-    fi
-
-# Install python3:
-if [ ! -n `which python3` ]
-    then
-        echo "Installing Python 3 ..."
-	$INSTALL python3
-    fi
-
-# Install Python virtual environments are installed:
-if [ ! -n `which virtuaenvwrapper.sh` ]
-    then
-        echo "Installing Python virtual environments ..."
-	$PIP_INSTALL virtualenvwrapper
+        $APT_INSTALL kicad-demo
     fi
 
 # Modify the 
