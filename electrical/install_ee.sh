61510f44ada58a02e9461f5a0acdeac4f999f2df
Wayne Gramlich <wayne@gramlich.net>
2020-03-24 12:40:24 -0700

    More script hacking...

--- electrical/install_ee.sh
+++ electrical/install_ee.sh
@@ -5,7 +5,7 @@ PROJECT_HOME=`pwd | sed s,/hbrc_ros_robot_platform.*,,g`
 HR2_HOME=$PROJECT_HOME/hbrc_ros_robot_platform
 
 # Force the repository to be up-to-date:
-(cd $HR2_HOME ; ./install_startup.sh)
+source $HR2_HOME/install_start.sh
 
 # Install the electrical specific installations:
 (cd $HR2_HOME/electrical ; ./install_ee_main.sh)
--- mechanical/install_me.sh
+++ mechanical/install_me.sh
@@ -5,7 +5,7 @@ PROJECT_HOME=`pwd | sed s,/hbrc_ros_robot_platform.*,,g`
 HR2_HOME=$PROJECT_HOME/hbrc_ros_robot_platform
 
 # Force the repository to be up-to-date:
-(cd $HR2_HOME ; ./install_startup.sh)
+source $HR2_HOME/install_start.sh
 
-# Install the mechancial specific installations:
-(cd $HR2_HOME/mechancial ; ./install_me_main.sh)
+# Install the mechanical specific installations:
+(cd $HR2_HOME/mechanical ; ./install_me_main.sh)
