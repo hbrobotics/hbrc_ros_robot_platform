e5a40f99b8714cde394f63ea304d3d2e0dcdbee7
James Nugen <jnugen@gmail.com>
2021-03-27 07:30:35 -0700

    Added 'software/hr2-encoders-test'

---
+++ software/hr2-encoders-test/Core/Src/lptim.c
@@ -0,0 +1,126 @@
+/**
+  ******************************************************************************
+  * @file    lptim.c
+  * @brief   This file provides code for the configuration
+  *          of the LPTIM instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "lptim.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+LPTIM_HandleTypeDef hlptim1;
+
+/* LPTIM1 init function */
+void MX_LPTIM1_Init(void)
+{
+
+  /* USER CODE BEGIN LPTIM1_Init 0 */
+
+  /* USER CODE END LPTIM1_Init 0 */
+
+  /* USER CODE BEGIN LPTIM1_Init 1 */
+
+  /* USER CODE END LPTIM1_Init 1 */
+  hlptim1.Instance = LPTIM1;
+  hlptim1.Init.Clock.Source = LPTIM_CLOCKSOURCE_APBCLOCK_LPOSC;
+  hlptim1.Init.Clock.Prescaler = LPTIM_PRESCALER_DIV1;
+  hlptim1.Init.UltraLowPowerClock.Polarity = LPTIM_CLOCKPOLARITY_RISING;
+  hlptim1.Init.UltraLowPowerClock.SampleTime = LPTIM_CLOCKSAMPLETIME_DIRECTTRANSITION;
+  hlptim1.Init.Trigger.Source = LPTIM_TRIGSOURCE_SOFTWARE;
+  hlptim1.Init.OutputPolarity = LPTIM_OUTPUTPOLARITY_HIGH;
+  hlptim1.Init.UpdateMode = LPTIM_UPDATE_IMMEDIATE;
+  hlptim1.Init.CounterSource = LPTIM_COUNTERSOURCE_EXTERNAL;
+  if (HAL_LPTIM_Init(&hlptim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN LPTIM1_Init 2 */
+
+  /* USER CODE END LPTIM1_Init 2 */
+
+}
+
+void HAL_LPTIM_MspInit(LPTIM_HandleTypeDef* lptimHandle)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(lptimHandle->Instance==LPTIM1)
+  {
+  /* USER CODE BEGIN LPTIM1_MspInit 0 */
+
+  /* USER CODE END LPTIM1_MspInit 0 */
+    /* LPTIM1 clock enable */
+    __HAL_RCC_LPTIM1_CLK_ENABLE();
+
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    /**LPTIM1 GPIO Configuration
+    PG12     ------> LPTIM1_IN1
+    PE1     ------> LPTIM1_IN2
+    */
+    GPIO_InitStruct.Pin = LQUAD_A_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF3_LPTIM1;
+    HAL_GPIO_Init(LQUAD_A_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = LQUAD_B_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF3_LPTIM1;
+    HAL_GPIO_Init(LQUAD_B_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN LPTIM1_MspInit 1 */
+
+  /* USER CODE END LPTIM1_MspInit 1 */
+  }
+}
+
+void HAL_LPTIM_MspDeInit(LPTIM_HandleTypeDef* lptimHandle)
+{
+
+  if(lptimHandle->Instance==LPTIM1)
+  {
+  /* USER CODE BEGIN LPTIM1_MspDeInit 0 */
+
+  /* USER CODE END LPTIM1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_LPTIM1_CLK_DISABLE();
+
+    /**LPTIM1 GPIO Configuration
+    PG12     ------> LPTIM1_IN1
+    PE1     ------> LPTIM1_IN2
+    */
+    HAL_GPIO_DeInit(LQUAD_A_GPIO_Port, LQUAD_A_Pin);
+
+    HAL_GPIO_DeInit(LQUAD_B_GPIO_Port, LQUAD_B_Pin);
+
+  /* USER CODE BEGIN LPTIM1_MspDeInit 1 */
+
+  /* USER CODE END LPTIM1_MspDeInit 1 */
+  }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
